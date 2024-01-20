fa99dddae21da4844a89dacf8ba97fcdaa2a1bfa
sharmaakhilesh1 <mailmeakilesh997@gmail.com>
2024-01-21 02:42:52 +0530

    profile-card

---
+++ script.js
@@ -0,0 +1,9 @@
+function showUserCard() {
+  var userCard = document.getElementById("userCard");
+  userCard.style.display = "block";
+}
+
+function hideUserCard() {
+  var userCard = document.getElementById("userCard");
+  userCard.style.display = "none";
+}
